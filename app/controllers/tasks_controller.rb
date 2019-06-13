c931847a4bf84a1995bfdc81aa1d581282ba3ee6
saburouz <saburouz.public@gmail.com>
2019-06-13 09:49:59 +0000

    CRUD tasts 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  
   def index
       @tasks = Task.all
   end
@@ -12,15 +13,15 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
-      
-      if @task.save
-          flash[:success] = 'Task が正常に登録されました'
-          redirect_to @task
-      else
-          flash.now[:danger] = 'Task が登録されませんでした'
-          render :new
-      end
+    @task = Task.new(task_params)
+    
+    if @task.save
+        flash[:success] = 'Task が正常に登録されました'
+        redirect_to @task
+    else
+        flash.now[:danger] = 'Task が登録されませんでした'
+        render :new
+    end
   end
 
   def edit
@@ -30,13 +31,13 @@ class TasksController < ApplicationController
   def update
       @task = Task.find(params[:id])
       
-      if @task.update(task_params)
-          flash[:success] = 'Task は正常に更新されました'
-          redirect_to @task
-      else
-          flash.now[:danger] = 'Task は更新されませんでした'
-          render :edit
-      end
+    if @task.update(task_params)
+        flash[:success] = 'Task は正常に更新されました'
+        redirect_to @task
+    else
+        flash.now[:danger] = 'Task は更新されませんでした'
+        render :edit
+    end
   end
 
   def destroy
@@ -46,10 +47,11 @@ class TasksController < ApplicationController
       flash[:success] = 'Task は正常に削除されました'
       redirect_to tasks_url
   end
-end
+  
+  private
+  
+  def task_params
+      params.require(:task).permit(:content)
+  end
 
-private
-
-def task_params
-    params.require(:task).permit(:content)
 end
