bfd14e74cd26efac5365607fd2e04ba0649eb9cb
saburouz <saburouz.public@gmail.com>
2019-06-15 03:36:28 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
       @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
   end
 
   def new
@@ -25,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
   end
 
   def update
-      @task = Task.find(params[:id])
       
     if @task.update(task_params)
         flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success] = 'Task は正常に削除されました'
@@ -51,7 +48,7 @@ class TasksController < ApplicationController
   private
   
   def set_message
-      @message = Message.find(params[:id])
+      @task = Task.find(params[:id])
   end
   
   def task_params
