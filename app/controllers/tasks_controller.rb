29e5790bf175f9559bf9b5c918cdefd68b990e40
saburouz <saburouz.public@gmail.com>
2019-07-03 10:37:16 +0000

    tasklist_login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
     before_action :set_message, only: [:show, :edit, :update, :destroy]
-  
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+    
   def index
-      @tasks = Task.all
+    if (logged_in?)
+      @tasks = current_user.tasks
+    end
   end
 
   def show
@@ -13,12 +17,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
         flash[:success] = 'Task が正常に登録されました'
         redirect_to @task
     else
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
         flash.now[:danger] = 'Task が登録されませんでした'
         render :new
     end
@@ -42,7 +47,7 @@ class TasksController < ApplicationController
       @task.destroy
       
       flash[:success] = 'Task は正常に削除されました'
-      redirect_to tasks_url
+      redirect_to root_url
   end
   
   private
@@ -54,5 +59,11 @@ class TasksController < ApplicationController
   def task_params
       params.require(:task).permit(:content, :status)
   end
-
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])  
+    unless @task
+      redirect_to signup_url
+    end
+  end
 end
